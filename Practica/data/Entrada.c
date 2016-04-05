e22979b428b79034b01bd794f57ef8a7b44242ad
David Heras <davidherasp@gmail.com>
2016-04-05 16:37:01 +0200

    Ultimos ficheros actualizados

---
+++ Practica/data/Entrada.c
@@ -0,0 +1,16 @@
+main()
+{ 
+//declaraciones
+	int a;
+	int b,casa23; 
+	float f;
+//codigo
+	printf("Introduce un entero\n"); scanf("%d",&a); 
+	if(a) {	
+		b=a+1000; 
+	}
+	else
+		f=a+23.5;
+	printf("Adiooooooooos"); 
+}
+//fin
